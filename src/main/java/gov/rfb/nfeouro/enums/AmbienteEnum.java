44d920e7163c423b663683ca019376432a269f6f
rafael <r_s_carvalho@yahoo.com.br>
2023-06-30 10:39:15 -0300

    Atualização da URL de Produção

--- src/main/java/gov/rfb/nfeouro/enums/AmbienteEnum.java
+++ src/main/java/gov/rfb/nfeouro/enums/AmbienteEnum.java
@@ -1,11 +1,12 @@
 package gov.rfb.nfeouro.enums;
 
 public enum AmbienteEnum {
-    HOMOLOGACAO("https://hom-nfoe.estaleiro.serpro.gov.br/API");
+    HOMOLOGACAO("https://hom-nfoe.estaleiro.serpro.gov.br/API"),
+    PRODUCAO("https://nfeouro.rfb.gov.br/API");
 
     AmbienteEnum(String valor) {
         url = valor;
     }
 
-    public String url;
+    public final String url;
 }
